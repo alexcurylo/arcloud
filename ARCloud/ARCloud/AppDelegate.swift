409a572a9b4c0867231797ce98c8dbb3faa2925a
Alex Curylo <alex@alexcurylo.com>
2017-10-27 05:39:19 +0300

    Add app setup

--- ARCloud/ARCloud/AppDelegate.swift
+++ ARCloud/ARCloud/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+ 
+        // https://cloud.estimote.com/#/apps
+        ESTConfig.setupAppID("arcloud-nn8", andAppToken:"cfe3e7b97fbb6245cb90a065f4ac0c64");
+
         return true
     }
 
